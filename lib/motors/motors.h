af834dcb354abcb401ef34fd47b1736ccbd62c0f
imirfurkan <furkan4ness@gmail.com>
2025-05-13 23:16:45 +0300

    new motor speed

--- lib/motors/motors.h
+++ lib/motors/motors.h
@@ -15,7 +15,7 @@ static constexpr uint8_t MOTOR_IN2_PIN[4] = {23, 25, 27, 29};
 // Per-motor “unloaded” scaling factors
 static constexpr float MOTOR_K_UNLOADED[4] = {0.974f, 0.808f, 0.835f, 0.750f};
 static constexpr bool MOTOR_FORWARD_DIR = true;
-static constexpr int MOTOR_SPEED_DEFAULT = 100; // PWM 0-255
+static constexpr int MOTOR_SPEED_DEFAULT = 150; // PWM 0-255
 static constexpr int MOTOR_PIVOT_SPEED = 50;
 static constexpr int MOTOR_GENTLE_SPEED = 80;
 static constexpr float MOTOR_STEER_PCT = 0.5f; // 0.0 = hard turn, 1.0 = straight
